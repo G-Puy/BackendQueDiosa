4fea4cf99651279fc8fa005dc784a2114ee0f4e7
Santiago Villar <santiagovh20@gmail.com>
2024-01-16 20:23:10 -0300

    TrearTodos AlertaStock

--- Dominio/Entidades/AlertaStock.cs
+++ Dominio/Entidades/AlertaStock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DTOS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,29 @@ using System.Threading.Tasks;
 
 namespace Dominio.Entidades
 {
-    internal class AlertaStock
+    public class AlertaStock
     {
-        public long Id{ get; set; } = 0;
+        public long Id { get; set; } = 0;
         public long IdStock { get; set; } = 0;
         public string Descripcion { get; set; } = "";
         public bool Leida { get; set; } = false;
+
+        public DTOAlertaStock darDto()
+        {
+            DTOAlertaStock alerta = new DTOAlertaStock();
+            alerta.Id = Id;
+            alerta.IdStock = IdStock;
+            alerta.Descripcion = Descripcion;
+            alerta.Leida = Leida;
+            return alerta;
+        }
+
+        public void cargarDeDto(DTOAlertaStock alerta)
+        {
+            Id = alerta.Id;
+            IdStock = alerta.IdStock;
+            Descripcion = alerta.Descripcion;
+            Leida = alerta.Leida;
+        }
     }
 }
--- Repositorios/RepositorioAlertaStock.cs
+++ Repositorios/RepositorioAlertaStock.cs
@@ -1,7 +1,10 @@
-﻿using DTOS;
+﻿using Conexiones;
+using Dominio.Entidades;
+using DTOS;
 using IRepositorios;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,9 @@ namespace Repositorios
 {
     public class RepositorioAlertaStock : RepositorioBase, IRepositorioAlertaStock
     {
+        private Conexion manejadorConexion = new Conexion();
+        private SqlConnection cn;
+
         public bool Alta(DTOAlertaStock obj)
         {
             throw new NotImplementedException();
@@ -37,7 +43,41 @@ namespace Repositorios
 
         public IEnumerable<DTOAlertaStock> TraerTodos()
         {
-            throw new NotImplementedException();
+            List<DTOAlertaStock> alertas = new List<DTOAlertaStock>();
+
+            cn = manejadorConexion.CrearConexion();
+            SqlTransaction trn = null;
+            try
+            {
+                string sentenciaSql = @"SELECT * FROM AlertaStock";
+                SqlCommand cmd = new SqlCommand(sentenciaSql, cn);
+                manejadorConexion.AbrirConexion(cn);
+                trn = cn.BeginTransaction();
+                cmd.Transaction = trn;
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        AlertaStock alerta = new AlertaStock();
+                        alerta.Id = Convert.ToInt64(reader["idAlertaStock"]);
+                        alerta.IdStock = Convert.ToInt64(reader["idStock"]);
+                        alerta.Descripcion = Convert.ToString(reader["descripcion"]);
+                        alerta.Leida = Convert.ToBoolean(reader["leida"]);
+                        DTOAlertaStock dtoTipoT = alerta.darDto();
+                        alertas.Add(dtoTipoT);
+                    }
+                }
+                trn.Rollback();
+                manejadorConexion.CerrarConexionConClose(cn);
+                return (IEnumerable<DTOAlertaStock>)alertas;
+            }
+            catch (Exception ex)
+            {
+                trn.Rollback();
+                manejadorConexion.CerrarConexionConClose(cn);
+                this.DescripcionError = ex.Message;
+                throw ex;
+            }
         }
     }
 }
